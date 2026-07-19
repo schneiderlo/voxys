b8c01417616e8b8fcd8acc76e1785a68bda6a822
schneiderlo <schneider.loic.mp@gmail.com>
2026-07-19 18:49:17 -0400

    Add Nix development environment pinned to 26.05

---
+++ flake.nix
@@ -0,0 +1,13 @@
+{
+  description = "Voxys native and WebAssembly development environment";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+
+  outputs = { nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      devShells.${system}.default = import ./shell.nix { inherit pkgs; };
+    };
+}
